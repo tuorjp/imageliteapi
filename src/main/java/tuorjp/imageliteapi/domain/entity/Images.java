deffa267352a77ba2e2dbb5c228fcd58cb3950c1
tuorjp <jplmarotinho22@gmail.com>
2024-04-24 11:52:05 -0300

    JPA first Entity Mapping (Image Entity)

---
+++ src/main/java/tuorjp/imageliteapi/domain/entity/Images.java
@@ -0,0 +1,55 @@
+package tuorjp.imageliteapi.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import tuorjp.imageliteapi.domain.enums.ImageExtension;
+
+import java.time.LocalDateTime;
+
+/*MAPEAMENTO OBJETO RELACIONAL:
+
+@Entity, @Table, @Column podem ou não receber argumentos, se não o nome da Entidade, tabela e coluna vão ser os mesmos da Classe ou atributo a que se referem
+@EntityListeners audita a data que foi feita a inserção no banco, precisa ativar na classe main pra funcionar
+@Data cria os getters, setters, toString, equalsAndHashCode e um construtor dos campos obrigatórios junto dos @NoArgsConstructor e @AllArgsConstructor
+@Builder precisa do @AllArgsConstructor para funcionar
+*/
+
+@Entity
+@Table
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Images {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Long size;
+
+    @Enumerated(EnumType.STRING)//grava o nome que está dentro do enum
+    //@Enumerated(EnumType.ORDINAL)//grava o índice que está dentro do enum
+    private ImageExtension extension;
+
+    @Column
+    @CreatedDate
+    private LocalDateTime uploadDate;
+
+    @Column
+    private String tags;
+
+    @Column
+    @Lob //diz que é um arquivo
+    private byte[] file;
+}
---
+++ src/main/java/tuorjp/imageliteapi/domain/enums/ImageExtension.java
@@ -0,0 +1,8 @@
+package tuorjp.imageliteapi.domain.enums;
+
+public enum ImageExtension {
+    PNG,
+    JPG,
+    GIF,
+    JPEG
+}
