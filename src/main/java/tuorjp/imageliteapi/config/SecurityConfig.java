ed51edcb48c0edd2581fc6995c23b9414d602c0b
JP <jplmarotinho22@gmail.com>
2024-07-31 16:32:11 -0300

    Individual image search permission

--- src/main/java/tuorjp/imageliteapi/config/SecurityConfig.java
+++ src/main/java/tuorjp/imageliteapi/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package tuorjp.imageliteapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -37,7 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                     //libera endpoint de login para todas as requisições
                     auth.requestMatchers("v1/users/**").permitAll();
-
+                    auth.requestMatchers(HttpMethod.GET, "v1/images/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
